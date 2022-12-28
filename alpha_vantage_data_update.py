b9bd8f0c38599ae472f6e98db8df7af8672a02ed
Aman Jaiswal <general@amanjaiswal.vip>
2022-12-28 10:47:46 -0500

    Changed scheduling to run everyday and automatically find the gap in the data

--- alpha_vantage_data_update.py
+++ alpha_vantage_data_update.py
@@ -4,53 +4,48 @@ from datetime import timedelta
 import time as Time
 from source.Commons import notify
 from source.AWS import *
-
+from config.config import logpath
 from source.Commons import upload_to_rds_table
-
-firstrun = True
-first = False
+from source.AWS import get_max_date
+from source.Commons import TradingDays, NearestTradingDay
+import os
 # This is used to update data everyday from Alpha Vantage
 os.system('sudo timedatectl set-timezone America/New_York')
 while True:
     # Defining Today's Date
     Today = str(datetime.today().date())
+    recent_date_rds = get_max_date('alpha_vantage')
+    before_market_close = datetime.now() < (datetime.now() + timedelta(days=0)).replace(hour=19, minute=30, second=0, microsecond=0)
+    if before_market_close:
+        recent_trading_day = NearestTradingDay(str((datetime.now() - timedelta(days=1)).date()))
+    else:
+        recent_trading_day = NearestTradingDay(Today)
     # Date that data will be available. Data is will not be available for today because all data is delayed by 1 day so
     # to reflect this discrepancy accurately we must also subtract 1 day from the current day and label all the data
     # with that date. For example if we are pulling data today it is not the data for the current date rather the data
     # for yesterday
     # Check if Today is a Trading Day by looking if it is in a list of all Trading Days until 2024 that is imported
-    if Today in TradingDays and not firstrun:
-        print('Starting Data Pull for', Today)
-        while True:
-            try:
-                if first == True:
-                    todaydata = alphavantage_nyse_nasdaq_download()
-                    pass
-                else:
-                    todaydata = alphavantage_nyse_nasdaq_download()  # Get today's Data from Alpha Vantage
-                    pass
-                break
-            except Exception as e:
-                print("Could not download data from alpha vantage on"+Today)
-                notify("Could not download data from alpha vantage on"+Today)
-                raise e
-        todaydata = todaydata[todaydata['date'] == Today]
-        upload_to_rds_table(todaydata,'alpha_vantage',row_by_row=True,save_errors=True)
+    start_date = str((datetime.strptime(recent_date_rds,'%Y-%m-%d')+timedelta(days=1)).date())
+    if recent_trading_day != recent_date_rds:
+        print('Starting Data Pull from', start_date, 'to', recent_trading_day)
+        try:
+            todaydata = alphavantage_nyse_nasdaq_download(end_date=recent_trading_day, start_date=start_date)
+        except Exception as e:
+            todaydata.to_csv(logpath+'todaydata_tiingo_'+Today+'.csv')
+            print("Could not download data from tiingo on"+Today)
+            notify("Could not download data from tiingo on"+Today)
+            raise e
+        upload_to_rds_table(todaydata,'alpha_vantage',row_by_row=False,save_errors=True)
     now = datetime.now()
     # Get the time we want to re-run the program at
-    if Today == str(datetime.today().date()) and not firstrun:
-        runAt = (datetime.now() + timedelta(days=1)).replace(hour=3, minute=0, second=0, microsecond=0)
-    elif datetime.now() < (datetime.now() + timedelta(days=0)).replace(hour=21, minute=30, second=0, microsecond=0) or firstrun:
-        runAt = (datetime.now() + timedelta(days=1)).replace(hour=21, minute=30, second=0, microsecond=0)
+    if datetime.now() > (datetime.now() + timedelta(days=0)).replace(hour=21, minute=30, second=0, microsecond=0):
+        runAt = (datetime.now() + timedelta(days=0)).replace(hour=21, minute=30, second=0, microsecond=0)
     else:
-        runAt = (datetime.now() + timedelta(seconds=5))
+        runAt = (datetime.now() + timedelta(days=1)).replace(hour=21, minute=30, second=0, microsecond=0)
     # Get the difference between the time we want to sleep at and the time right now
     delta = (runAt - now).total_seconds()
     print('\nFinished Task for', Today + '. Going to sleep for,', str(round((int(delta) / 60) / 60, 2)) + ' hours.\n')
     # Sleep for the difference between the time now and the time we want to run at again where it will resume and go to
     # Start of the while loop again
-    firstrun = False
-    if not firstrun:
-        first = False
     Time.sleep(delta)
 
--- tiingo_data_update.py
+++ tiingo_data_update.py
@@ -4,52 +4,49 @@ from datetime import timedelta
 import time as Time
 from source.Commons import notify
 from source.AWS import *
-from source.Commons import upload_to_rds_table
 from config.config import logpath
-firstrun = True
-first = False
+from source.Commons import upload_to_rds_table
+from source.AWS import get_max_date
+from source.Commons import TradingDays, NearestTradingDay
+import os
 # This is used to update data everyday from tiingo
 os.system('sudo timedatectl set-timezone America/New_York')
 while True:
     # Defining Today's Date
     Today = str(datetime.today().date())
+    recent_date_rds = get_max_date('tiingo')
+    before_market_close = datetime.now() < (datetime.now() + timedelta(days=0)).replace(hour=19, minute=30, second=0,microsecond=0)
+    if before_market_close:
+        recent_trading_day = NearestTradingDay(str((datetime.now() - timedelta(days=1)).date()))
+    else:
+        recent_trading_day = NearestTradingDay(Today)
     # Date that data will be available. Data is will not be available for today because all data is delayed by 1 day so
     # to reflect this discrepancy accurately we must also subtract 1 day from the current day and label all the data
     # with that date. For example if we are pulling data today it is not the data for the current date rather the data
     # for yesterday
     # Check if Today is a Trading Day by looking if it is in a list of all Trading Days until 2024 that is imported
-    if Today in TradingDays and not firstrun:
-        print('Starting Data Pull from tiingo on', Today)
-        while True:
-            try:
-                if first == True:
-                    todaydata = tiingo_nyse_nasdaq_download('tiingo', end_date=Today, days=365*60)
-                    pass
-                else:
-                    todaydata = tiingo_nyse_nasdaq_download('tiingo', end_date=Today,days=Today)  # Get today's Data from tiingo
-                    pass
-                break
-            except Exception as e:
-                todaydata.to_csv(log+'todaydata_tiingo_'+Today+'.csv')
-                print("Could not download data from tiingo on"+Today)
-                notify("Could not download data from tiingo on"+Today)
-                raise e
+    start_date = str((datetime.strptime(recent_date_rds, '%Y-%m-%d') + timedelta(days=1)).date())
+    if recent_trading_day != recent_date_rds:
+        print('Starting Data Pull from', start_date, 'to', recent_trading_day)
+        try:
+            todaydata = tiingo_nyse_nasdaq_download(end_date=recent_trading_day, start_date=start_date)
+        except Exception as e:
+            todaydata.to_csv(logpath+'todaydata_tiingo_'+Today+'.csv')
+            print("Could not download data from tiingo on"+Today)
+            notify("Could not download data from tiingo on"+Today)
+            raise e
         todaydata.to_csv(logpath+'todaydata_tiingo_' + Today + '.csv')
-        upload_to_rds_table(todaydata,'tiingo',row_by_row=True,save_errors=True)
+        upload_to_rds_table(todaydata,'tiingo',row_by_row=False,save_errors=True)
+    now = datetime.now()
     now = datetime.now()
     # Get the time we want to re-run the program at
-    if Today == str(datetime.today().date()) and firstrun == False:
-        runAt = (datetime.now() + timedelta(days=1)).replace(hour=20, minute=30, second=0, microsecond=0)
-    elif datetime.now() < (datetime.now() + timedelta(days=0)).replace(hour=20, minute=30, second=0, microsecond=0):
-        runAt = (datetime.now() + timedelta(days=0)).replace(hour=20, minute=30, second=0, microsecond=0)
+    if datetime.now() > (datetime.now() + timedelta(days=0)).replace(hour=21, minute=30, second=0, microsecond=0):
+        runAt = (datetime.now() + timedelta(days=0)).replace(hour=21, minute=30, second=0, microsecond=0)
     else:
-        runAt = (datetime.now() + timedelta(seconds=5))
+        runAt = (datetime.now() + timedelta(days=1)).replace(hour=21, minute=30, second=0, microsecond=0)
     # Get the difference between the time we want to sleep at and the time right now
     delta = (runAt - now).total_seconds()
     print('\nFinished Task for', Today + '. Going to sleep for,', str(round((int(delta) / 60) / 60, 2)) + ' hours.\n')
     # Sleep for the difference between the time now and the time we want to run at again where it will resume and go to
     # Start of the while loop again
-    firstrun = False
-    first = False
     Time.sleep(delta)
-
